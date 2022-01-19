3c6c478b373095f34e41114ed61cbe532a14c424
Kishlin <hello@pierrelouislegrand.com>
2022-01-19 12:14:57 +0100

    Hotfix: Update DomainEvents to use UuidValueObject as an aggregate Uuid

--- src/Backend/Shared/Domain/Bus/Event/DomainEvent.php
+++ src/Backend/Shared/Domain/Bus/Event/DomainEvent.php
@@ -4,28 +4,18 @@ declare(strict_types=1);
 
 namespace Kishlin\Backend\Shared\Domain\Bus\Event;
 
+use Kishlin\Backend\Shared\Domain\ValueObject\UuidValueObject;
+
 abstract class DomainEvent
 {
     public function __construct(
-        private string  $aggregateUuid,
-        private ?string $eventUuid,
-        private ?string $occurredOn,
+        private UuidValueObject $aggregateUuid,
     ) {}
 
     abstract public static function eventName(): string;
 
-    public function aggregateUuid(): string
+    public function aggregateUuid(): UuidValueObject
     {
         return $this->aggregateUuid;
-    }
-
-    public function eventUuid(): ?string
-    {
-        return $this->eventUuid;
-    }
-
-    public function occurredOn(): ?string
-    {
-        return $this->occurredOn;
     }
 }
