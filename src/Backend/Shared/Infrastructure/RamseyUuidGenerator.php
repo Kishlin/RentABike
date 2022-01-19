9183b0c89aec2bedc191127c5f741f2bcb694135
Kishlin <hello@pierrelouislegrand.com>
2022-01-19 12:53:01 +0100

    Hotfix: Make the Ramsey Uuid Generator final

--- src/Backend/Shared/Infrastructure/RamseyUuidGenerator.php
+++ src/Backend/Shared/Infrastructure/RamseyUuidGenerator.php
@@ -7,7 +7,7 @@ namespace Kishlin\Backend\Shared\Infrastructure;
 use Kishlin\Backend\Shared\Domain\UuidGenerator;
 use Ramsey\Uuid\Uuid;
 
-class RamseyUuidGenerator implements UuidGenerator
+final class RamseyUuidGenerator implements UuidGenerator
 {
     public function generate(): string
     {
