aabcdd02c3e2cabcafc0cc409942406658b9e0ff
Kishlin <hello@pierrelouislegrand.com>
2022-01-19 18:25:59 +0100

    Tests: Backoffice - Implement the Core Database Service Test

---
+++ apps/backoffice/backend/tests/IntegrationTests/Doctrine/CoreDatabaseServiceTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kishlin\Tests\Apps\Backoffice\Backend\IntegrationTests\Doctrine;
+
+use Doctrine\DBAL\Exception;
+use Kishlin\Tests\Backend\Apps\AbstractIntegrationTests\Doctrine\CoreDatabaseServiceTestCase;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+
+final class CoreDatabaseServiceTest extends CoreDatabaseServiceTestCase
+{
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     * @throws Exception
+     */
+    public function testItHasAnActiveDatabaseConnection(): void
+    {
+        self::assertItHasAnActiveDatabaseConnection($this->getContainer());
+    }
+}
